b493fe6f3539bc5e42c5c87cd233ce0c456d2ab7
quentin452 <42176772+quentin452@users.noreply.github.com>
2024-02-22 07:44:02 +0100

    Add alot of profilers using JPROF + up conf.lua

--- conf.lua
+++ conf.lua
@@ -3,4 +3,6 @@ function love.conf(t)
     t.window.title = "g3d demo"
     t.version = "11.5"
     t.window.vsync = 1
+    t.window.width = 1280
+	t.window.height = 720
 end
--- main.lua
+++ main.lua
@@ -1,6 +1,8 @@
 -- written by groverbuger for g3d
 -- september 2021
 -- MIT license
+PROF_CAPTURE = true
+prof = require("jprofiler/jprof")
 
 local g3d = require "g3d"
 local earth = g3d.newModel("assets/sphere.obj", "assets/earth.png", {4,0,0})
@@ -9,6 +11,9 @@ local background = g3d.newModel("assets/sphere.obj", "assets/starfield.png", nil
 local timer = 0
 
 function love.update(dt)
+    prof.push("frame")
+    prof.push("MainUpdate")
+    love.filesystem.setIdentity("G3D")
     timer = timer + dt
     moon:setTranslation(math.cos(timer)*5 + 4, math.sin(timer)*5, 0)
     moon:setRotation(0, 0, timer - math.pi/2)
@@ -16,14 +21,28 @@ function love.update(dt)
     if love.keyboard.isDown "escape" then
         love.event.push "quit"
     end
+    prof.pop("MainUpdate")
+    prof.pop("frame")
 end
 
 function love.draw()
+    prof.push("frame")
+    prof.push("MainDraw")
     earth:draw()
     moon:draw()
     background:draw()
+    prof.pop("MainDraw")
+    prof.pop("frame")
 end
 
 function love.mousemoved(x,y, dx,dy)
+    prof.push("frame")
+    prof.push("Mainmousemoved")
     g3d.camera.firstPersonLook(dx,dy)
+    prof.pop("Mainmousemoved")
+    prof.pop("frame")
+end
+
+function love.quit()
+	prof.write("prof.mpack")
 end
